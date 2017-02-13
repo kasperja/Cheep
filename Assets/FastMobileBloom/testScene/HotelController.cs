ed64292cb9a6db96fe9ec03f0a59d6729220c054
kasperja <kasperjensenandersen@gmail.com>
2017-02-13 12:08:37 +0100

    bloom effect, highscore WIP, Trail
    bloom effect, highscore WIP, Trail

---
+++ Assets/FastMobileBloom/testScene/HotelController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class HotelController : MonoBehaviour
+{
+	public UnityStandardAssets.ImageEffects.FastMobileBloom myBloomScript;
+	public void SwitchBloom()
+	{
+		myBloomScript.enabled = !myBloomScript.enabled;
+	}
+}
--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -7,6 +7,15 @@ public class GameOverScript : MonoBehaviour {
 	int score = 0;
 
 	public int highscore = 0;
+	public int highscore2 = 0;
+	public int highscore3 = 0;
+	public int highscore4 = 0;
+	public int highscore5 = 0;
+	public int highscore6 = 0;
+	public int highscore7 = 0;
+	public int highscore8 = 0;
+	public int highscore9 = 0;
+	public int highscore10 = 0;
 
 	public Text yourScoreIsTxt;
 
@@ -35,17 +44,176 @@ public class GameOverScript : MonoBehaviour {
 		if(ES2.Exists("myHighscore")){
 
 			highscore = ES2.Load<int> ("myHighscore");
+			if(ES2.Exists("myHighscore2"))highscore2 = ES2.Load<int> ("myHighscore2");
+			if(ES2.Exists("myHighscore3"))highscore3 = ES2.Load<int> ("myHighscore3");
+			if(ES2.Exists("myHighscore4"))highscore4 = ES2.Load<int> ("myHighscore4");
+			if(ES2.Exists("myHighscore5"))highscore5 = ES2.Load<int> ("myHighscore5");
+			if(ES2.Exists("myHighscore6"))highscore6 = ES2.Load<int> ("myHighscore6");
+			if(ES2.Exists("myHighscore7"))highscore7 = ES2.Load<int> ("myHighscore7");
+			if(ES2.Exists("myHighscore8"))highscore8 = ES2.Load<int> ("myHighscore8");
+			if(ES2.Exists("myHighscore9"))highscore9 = ES2.Load<int> ("myHighscore9");
+			if(ES2.Exists("myHighscore10"))highscore10 = ES2.Load<int> ("myHighscore10");
 
 			if (score > highscore) {
 
 				yourScoreIsTxt.text = "It's a new record!";
 
 				highscore = score;
+
 				ES2.Save (score, "myHighscore");
 
 
 
-			} else {
+			} else if(score > highscore2){
+
+
+
+				highscore10 = highscore9;
+				highscore9 = highscore8;
+				highscore8 = highscore7;
+				highscore7 = highscore6;
+				highscore6 = highscore5;
+				highscore5 = highscore4;
+				highscore4 = highscore3;
+				highscore3 = highscore2;
+
+				highscore2 = score;
+
+				ES2.Save (score, "myHighscore2");
+				ES2.Save (score, "myHighscore3");
+				ES2.Save (score, "myHighscore4");
+				ES2.Save (score, "myHighscore5");
+				ES2.Save (score, "myHighscore6");
+				ES2.Save (score, "myHighscore7");
+				ES2.Save (score, "myHighscore8");
+				ES2.Save (score, "myHighscore9");
+				ES2.Save (score, "myHighscore10");
+
+
+
+			}else if(score > highscore3){
+
+				highscore10 = highscore9;
+				highscore9 = highscore8;
+				highscore8 = highscore7;
+				highscore7 = highscore6;
+				highscore6 = highscore5;
+				highscore5 = highscore4;
+				highscore4 = highscore3;
+
+				highscore3 = score;
+
+				ES2.Save (score, "myHighscore3");
+				ES2.Save (score, "myHighscore4");
+				ES2.Save (score, "myHighscore5");
+				ES2.Save (score, "myHighscore6");
+				ES2.Save (score, "myHighscore7");
+				ES2.Save (score, "myHighscore8");
+				ES2.Save (score, "myHighscore9");
+				ES2.Save (score, "myHighscore10");
+
+
+
+			}else if(score > highscore4){
+
+
+				highscore10 = highscore9;
+				highscore9 = highscore8;
+				highscore8 = highscore7;
+				highscore7 = highscore6;
+				highscore6 = highscore5;
+				highscore5 = highscore4;
+
+				highscore4 = score;
+
+				ES2.Save (score, "myHighscore4");
+				ES2.Save (score, "myHighscore5");
+				ES2.Save (score, "myHighscore6");
+				ES2.Save (score, "myHighscore7");
+				ES2.Save (score, "myHighscore8");
+				ES2.Save (score, "myHighscore9");
+				ES2.Save (score, "myHighscore10");
+
+
+			}else if(score > highscore5){
+
+
+				highscore10 = highscore9;
+				highscore9 = highscore8;
+				highscore8 = highscore7;
+				highscore7 = highscore6;
+				highscore6 = highscore5;
+
+				highscore5 = score;
+
+				ES2.Save (score, "myHighscore5");
+				ES2.Save (score, "myHighscore6");
+				ES2.Save (score, "myHighscore7");
+				ES2.Save (score, "myHighscore8");
+				ES2.Save (score, "myHighscore9");
+				ES2.Save (score, "myHighscore10");
+
+
+			}else if(score > highscore6){
+
+				highscore10 = highscore9;
+				highscore9 = highscore8;
+				highscore8 = highscore7;
+				highscore7 = highscore6;
+
+				highscore6 = score;
+
+				ES2.Save (score, "myHighscore6");
+				ES2.Save (score, "myHighscore7");
+				ES2.Save (score, "myHighscore8");
+				ES2.Save (score, "myHighscore9");
+				ES2.Save (score, "myHighscore10");
+
+
+
+			}else if(score > highscore7){
+
+				highscore10 = highscore9;
+				highscore9 = highscore8;
+				highscore8 = highscore7;
+
+				highscore7 = score;
+
+				ES2.Save (score, "myHighscore7");
+				ES2.Save (score, "myHighscore8");
+				ES2.Save (score, "myHighscore9");
+				ES2.Save (score, "myHighscore10");
+
+			}else if(score > highscore8){
+
+				highscore10 = highscore9;
+				highscore9 = highscore8;
+
+				highscore8 = score;
+
+				ES2.Save (score, "myHighscore8");
+				ES2.Save (score, "myHighscore9");
+				ES2.Save (score, "myHighscore10");
+
+
+			}else if(score > highscore9){
+
+				highscore10 = highscore9;
+
+				highscore9 = score;
+
+				ES2.Save (score, "myHighscore9");
+				ES2.Save (score, "myHighscore10");
+
+
+			}else if(score > highscore10){
+
+				highscore10 = score;
+
+				ES2.Save (score, "myHighscore10");
+
+
+			}else {
 			
 				yourRecordIsTxt.gameObject.SetActive (true);
 				//highScoreTxt.gameObject.SetActive (true);
