59bf2c90bb143e6979077a1521c824f645aa9d20
kasperja <kasperjensenandersen@gmail.com>
2017-03-02 11:39:05 +0100

    countdown added
    countdown added

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour {
+
+	public TextMesh textM;
+	public AudioSource countdownSound;
+	public AudioSource beginSound;
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine (countdownNum ());
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator countdownNum(){
+	
+		countdownSound.Play ();
+		textM.text = "3";
+		yield return new WaitForSeconds (1f);
+		countdownSound.Play ();
+		textM.text = "2";
+		yield return new WaitForSeconds (1f);
+		countdownSound.Play ();
+		textM.text = "1";
+		yield return new WaitForSeconds (1f);
+		beginSound.Play ();
+		textM.text = "GO!";
+		yield return new WaitForSeconds (1f);
+		textM.text = " ";
+
+	
+	}
+}
