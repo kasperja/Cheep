8eca2a34abc29fcede96a5fa56ed0067951e8b7e
kasperja <kasperjensenandersen@gmail.com>
2017-03-02 16:41:33 +0100

    Optimization
    Optimization

---
+++ Assets/Scripts/TrailSortOrder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailSortOrder : MonoBehaviour {
+
+	// Use this for initialization
+	public TrailRenderer trail;
+	// Use this for initialization
+	void Start () {
+		
+		trail.sortingOrder = 7;
+
+	}
+}
