b22f048ac2603afe8d32e43c3871f36b3d1108bf
kasperja <kasperjensenandersen@gmail.com>
2017-03-09 09:32:40 +0100

    Tree rotation, bug fixes
    Tree rotation, bug fixes

--- Assets/Scripts/TreeRotation.cs
+++ Assets/Scripts/TreeRotation.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class TreeRotation : MonoBehaviour {
 
 	//public RotateCam camRotateScript;
-	public Transform target;
+	private Transform target;
 	//public float speed = 10f;
+	//public RotateCam rCam;
 	// Use this for initialization
 	void Start () {
+
+		target = GameObject.Find ("Main Camera").GetComponent<Transform>();
+
+		//rCam = target.GetComponent<RotateCam> ();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		transform.rotation = new Quaternion(0f, 0f, target.rotation.z - target.rotation.z / 10f, 1f);
 		//transform.LookAt (new Vector3(transform.position.x, transform.position.z + 1000000f, -transform.position.y), Vector3.up);
 		//transform.RotateAround(gameObject.transform.position, Vector3.forward, target.transform.rotation.z);
 
