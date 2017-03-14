17805b28205d8e99f94708416ddfcdf6182ce49d
kasperja <kasperjensenandersen@gmail.com>
2017-03-14 11:55:23 +0100

    Snow on start and game over
    Snow on start and game over

--- Assets/Scripts/LavineMove.cs
+++ Assets/Scripts/LavineMove.cs
@@ -35,6 +35,7 @@ public class LavineMove : MonoBehaviour {
 
 	private bool isDeadByLavine = false;
 
+	public bool lavineTest = false;
 
 	// Use this for initialization
 	void Start () {
@@ -129,7 +130,7 @@ public class LavineMove : MonoBehaviour {
 
 
 
-			if (deactivateOnce && !pc2D.isDead) {
+			if (deactivateOnce && !pc2D.isDead && !isDeadByLavine) {
 				
 				Debug.Log ("deactivated");
 				//Handheld.Vibrate();
@@ -147,13 +148,13 @@ public class LavineMove : MonoBehaviour {
 			}
 
 			touchCounter = 0;
-			if (transform.position.x > beginPos.position.x) {
+			if (transform.position.x > beginPos.position.x && !isDeadByLavine) {
 				if (speed < 9f) {
 				
 					transform.position -= new Vector3 (5f, 0, 0) * Time.deltaTime;
 					//transform.position = Vector3.MoveTowards (transform.position, endPosDeath.position, speed * 2f * Time.deltaTime);
 
-				} else {
+				} else if(!isDeadByLavine){
 
 					transform.position -= new Vector3 (5f, 0, 0) * Time.deltaTime;
 					//transform.position = Vector3.MoveTowards (transform.position, beginPos.position, speed * Time.deltaTime);
@@ -255,8 +256,11 @@ public class LavineMove : MonoBehaviour {
 
 	IEnumerator setRollingTrue(){
 	
-		yield return new WaitForSeconds (0f/*Random.Range(25f,35f)*/);
-
+		if (lavineTest) {
+			yield return new WaitForSeconds (0f);
+		} else {
+			yield return new WaitForSeconds (Random.Range (25f, 35f));
+		}
 		rollOnce = true;
 
 		StartCoroutine (waitForRoll ());
