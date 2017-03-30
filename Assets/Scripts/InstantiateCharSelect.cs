0402e5122e26d602df861b05eb4463a6071250cc
kasperja <kasperjensenandersen@gmail.com>
2017-03-30 15:53:56 +0200

    char select working android
    char select working android

---
+++ Assets/Scripts/InstantiateCharSelect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateCharSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		GameObject charMenu = (GameObject)Instantiate(Resources.Load("CharSelectMenu")) as GameObject;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
