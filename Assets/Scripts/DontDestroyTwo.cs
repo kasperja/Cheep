511dcbdc3995eaed56a62a5db8dffba49efa1a89
kasperja <kasperjensenandersen@gmail.com>
2017-04-21 10:24:22 +0200

    music disabled button in menu
    music disabled button in menu

--- Assets/Scripts/DontDestroyTwo.cs
+++ Assets/Scripts/DontDestroyTwo.cs
@@ -10,7 +10,13 @@ public class DontDestroyTwo : MonoBehaviour {
 	private Scene currentScene; 
 	public static DontDestroyTwo instanceTwo;
 
+	private bool musicDisabled = false;
+
+	public AudioSource music;
+
 	void Awake(){
+
+		MusicDisable ();
 		
 		DontDestroyOnLoad (gameObject);
 
@@ -55,6 +61,12 @@ public class DontDestroyTwo : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
+		if (ES2.Exists ("musicDisabled")) {
+
+			musicDisabled = ES2.Load<bool> ("musicDisabled");
+
+		}
+
 		Scene currentScene = SceneManager.GetActiveScene();
 
 		if (currentScene.name == "GameOverScene") {
@@ -70,6 +82,12 @@ public class DontDestroyTwo : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+
+		if (ES2.Exists ("musicDisabled")) {
+
+			musicDisabled = ES2.Load<bool> ("musicDisabled");
+
+		}
 
 		currentScene = SceneManager.GetActiveScene();
 
@@ -91,15 +109,44 @@ public class DontDestroyTwo : MonoBehaviour {
 
 		}
 
-		if (audioFadeOut) {
+		if (musicDisabled) {
+
+			gameObject.GetComponent<AudioSource> ().volume = 0f;
+
+			//ES2.Save (musicDisabled, "musicDisabled");
+
+		}else if (audioFadeOut && !musicDisabled) {
 
 			if(gameObject.GetComponent<AudioSource> ().volume > 0f)gameObject.GetComponent<AudioSource> ().volume -= 1f * Time.deltaTime;
 
-		} else {
+		} else if(!musicDisabled) {
 
 			if(gameObject.GetComponent<AudioSource> ().volume < 1f)gameObject.GetComponent<AudioSource> ().volume += 1f * Time.deltaTime;
 
 		}
+	}
+
+	public void MusicDisable () {
+
+		music = gameObject.GetComponent<AudioSource> ();
+
+		if (ES2.Exists ("musicDisabled")) {
+
+			musicDisabled = ES2.Load<bool> ("musicDisabled");
+
+		}
+
+		if (musicDisabled) {
+
+			music.volume = 0f;;
+
+		} else {
+
+			music.volume = 1f;
+
+		}
+		//ES2.Save (musicDisabled, "musicDisabled");
+
 	}
 
 	IEnumerator waitAndDestroy(){
---
+++ Assets/Scripts/MusicButton.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicButton : MonoBehaviour {
+
+	private bool musicDisabled = false;
+
+	//public MusicController mController;
+	// Use this for initialization
+	void Start () {
+
+		if (ES2.Exists ("musicDisabled")) {
+
+			musicDisabled = ES2.Load<bool> ("musicDisabled");
+
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DisableMusic(){
+	
+		if (!musicDisabled) {
+			
+			musicDisabled = true;
+
+		} else {
+		
+			musicDisabled = false;
+		
+		}
+	
+		ES2.Save (musicDisabled, "musicDisabled");
+	
+		//mController.MusicDisable ();
+
+	}
+}
