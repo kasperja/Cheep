b7481a1d7b2c85bbd1f374ac5078a619cfe6dc0d
kasperja <kasperjensenandersen@gmail.com>
2017-05-08 10:32:55 +0200

    rocket boost countdown
    rocket boost countdown

--- Assets/Scripts/PowerupRocket.cs
+++ Assets/Scripts/PowerupRocket.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PowerupRocket : MonoBehaviour {
 
 	private PlatformerCharacter2D pc2D;
-
+	private Countdown countdown;
 
 	void Start(){
 	
 		pc2D = GameObject.Find ("CharacterRobotBoy").GetComponent<PlatformerCharacter2D> ();
+		countdown = GameObject.Find ("Countdown").GetComponent<Countdown> ();
 	
 	}
 
@@ -23,11 +24,20 @@ public class PowerupRocket : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other){
 
 		if (other.tag == "Player") {
-
-
+			
 			pc2D.isRocket = true;
-			Destroy (this.gameObject);
+			countdown.isRocket = true;
+
+			StartCoroutine (waitRocket ());
+
+			GetComponent<SpriteRenderer> ().enabled = false;
 
 		}
+	}
+	IEnumerator waitRocket(){
+	
+		yield return new WaitForSeconds (5f);
+
+		Destroy (this.gameObject);
 	}
 }
