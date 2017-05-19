d3ad5cb23d815e8647e8c39abbcc2a8986a5a2cb
kasperja <kasperjensenandersen@gmail.com>
2017-05-19 12:52:02 +0200

    MG tree updates, mg spikes and mg added
    MG tree updates, mg spikes and mg added

---
+++ Assets/Scripts/RandomXFlip.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomXFlip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		float random = Random.Range (0,1);
+		if (random <= 0.5f) {
+			GetComponent<SpriteRenderer> ().flipX = true;
+		} else {
+		
+		
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
