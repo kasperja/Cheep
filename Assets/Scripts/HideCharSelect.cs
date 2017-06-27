21ca12eb3fc085d8eaf44efad6e6467177b3a611
kasperja <kasperjensenandersen@gmail.com>
2017-06-27 11:22:39 +0200

    Squashes on landing on all characters, New popup almost done
    Squashes on landing on all characters, New popup almost done

---
+++ Assets/Scripts/HideCharSelect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCharSelect : MonoBehaviour {
+
+    public GameObject popupObj;
+    public GameObject menuObj;
+    public GameObject xButtonTopObj;
+	// Use this for initialization
+	void Start () {
+
+        //menuObj = GameObject.Find("Scroll View");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (popupObj.activeSelf)
+        {
+
+            menuObj.SetActive(true);
+            xButtonTopObj.SetActive(false);
+
+
+        }
+        else {
+            menuObj.SetActive(false);
+            xButtonTopObj.SetActive(true);
+
+
+        }
+		
+	}
+}
